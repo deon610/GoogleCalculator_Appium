15140972365356047528dcd863d537f5fab65dad
Deon Dsouza <34719316+deon610@users.noreply.github.com>
2020-11-10 03:31:55 +0530

    Added comments
    Comments added to the base class and base object

--- src/test/java/com/googlecalculator/base/BaseClass.java
+++ src/test/java/com/googlecalculator/base/BaseClass.java
@@ -21,14 +21,17 @@ import io.appium.java_client.remote.MobileCapabilityType;
 
 public class BaseClass
 {
+	//Instance of driver that drive the test cases running
 	public static AppiumDriver<MobileElement> driver;
 	public static Properties prop;
-	
-	public BaseClass() {
+
+	public BaseClass()
+	{
 		prop = new Properties();
 		String currentPath = Paths.get(".").toAbsolutePath().normalize().toString();
 		String propertiesPath = currentPath + "\\src\\test\\resources\\config.properties";
-		FileInputStream fis=null;;
+		FileInputStream fis = null;
+		;
 		try
 		{
 			fis = new FileInputStream(propertiesPath);
@@ -46,21 +49,33 @@ public class BaseClass
 			e.printStackTrace();
 		}
 	}
-	
+
 	@BeforeTest
-	public void initialize() throws MalformedURLException {
+	public void initialize() throws MalformedURLException
+	{
+		DesiredCapabilities desCap = new DesiredCapabilities();
+		initializeDesiredCapabilities(desCap);
+		String server = prop.getProperty("url");
+		URL url = new URL(server);
+		driver = new AppiumDriver<MobileElement>(url, desCap);
+	}
+	
+    /**
+     * Method to initialize the required Desired Capabilities
+     *
+     * @param 	desiredCapabilities		Instance of desired Capabilities passed
+     */
+	private void initializeDesiredCapabilities(DesiredCapabilities desCap) {
 		String currentPath = Paths.get(".").toAbsolutePath().normalize().toString();
 		String deviceName = prop.getProperty("deviceName");
 		String udid = prop.getProperty("udid");
 		String platformName = prop.getProperty("platformName");
 		String platformVersion = prop.getProperty("platformVersion");
 		String timeout = prop.getProperty("implicitwait");
-		String appPath = currentPath+ "\\src\\test\\resources\\"+ prop.getProperty("app");
+		String appPath = currentPath + "\\src\\test\\resources\\" + prop.getProperty("app");
 		String appPackage = prop.getProperty("appPackage");
 		String appActivity = prop.getProperty("appActivity");
-		String server = prop.getProperty("url");
-		
-		DesiredCapabilities desCap = new DesiredCapabilities();
+	
 		desCap.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
 		desCap.setCapability(MobileCapabilityType.UDID, udid);
 		desCap.setCapability(MobileCapabilityType.PLATFORM_NAME, platformName);
@@ -69,19 +84,18 @@ public class BaseClass
 		desCap.setCapability("appPackage", appPackage);
 		desCap.setCapability("appActivity", appActivity);
 		desCap.setCapability(MobileCapabilityType.APP, appPath);
-		
-		URL url = new URL(server);
-		driver = new AppiumDriver<MobileElement>(url,desCap);
 	}
-	
-	  @AfterMethod(alwaysRun = true)
-	    public void afterMethod(final ITestResult result) throws IOException {
-	        String fileName = result.getTestClass().getName() + "_" + result.getName();
-	        System.out.println("Test Case:" + fileName + " has executed");
-	    }
-	
+
+	@AfterMethod(alwaysRun = true)
+	public void afterMethod(final ITestResult result) throws IOException
+	{
+		String fileName = result.getTestClass().getName() + "_" + result.getName();
+		System.out.println("Test Case:" + fileName + " has executed");
+	}
+
 	@AfterTest
-	public void tearDown() {
+	public void tearDown()
+	{
 		driver.quit();
 	}
 }
--- src/test/java/com/googlecalculator/base/BasePageObject.java
+++ src/test/java/com/googlecalculator/base/BasePageObject.java
@@ -13,6 +13,20 @@ public class BasePageObject
 	public static final int Implicit_Wait = 10;
 	protected final AppiumDriver<MobileElement> driver;
 	
+	/**
+	  * A base page object class that is used to initialize the page object elements
+     *
+     * The page object model is used where we separate out the classes as per the screens
+     * present in the application. The specific page objects and functions are kept in 
+     * screen with intention to manage the maintainability
+     *
+     * The AppiumFieldDecorator class used within the page factory allows us to conveniently
+     * create an Android Element object using the @AndroidFindBy annotation within the pages extending this class.
+     *
+     * @param 	driver	The instance of appium driver that we are passing to this class
+     *  from the page class extending it. This driver is initialized in the Base class before
+     *  a test represented by the testNG annotation
+     */
 	public BasePageObject(AppiumDriver<MobileElement> driver) {
 		this.driver = driver;
 		initElements();
